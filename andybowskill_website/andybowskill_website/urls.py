d1758a1b761ec490b5a204c3aea0a0ec37a1b3b6
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-01-22 16:47:46 +0000

    Add various static files and start to use them in index.html.

--- andybowskill_website/andybowskill_website/urls.py
+++ andybowskill_website/andybowskill_website/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import template
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="index.html"), name="index")
 ]
