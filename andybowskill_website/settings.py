5712a2a312d1d768942e9f51252c2772d889601a
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-02-18 11:05:27 +0000

    Update the CI settings.

--- andybowskill_website/settings.py
+++ andybowskill_website/settings.py
@@ -148,4 +148,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
+
+# Test runner for CI
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+TEST_OUTPUT_VERBOSE = 2
+TEST_OUTPUT_DIR = 'test-results'
+
+
 django_heroku.settings(locals())
