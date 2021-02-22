7e4653cc502786daac1bf44d54d9be560a8345d0
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-02-22 10:49:32 +0000

    Add an environment variable instead of changing the setting.py file.

--- andybowskill_website/settings.py
+++ andybowskill_website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default="!&l77dbnf8x^(il(8gqqbp531#fcwpm@5n+mdp*wxsv4szi*u^")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
