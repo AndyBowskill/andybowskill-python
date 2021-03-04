43ce6d0c5285c61c37ca373d28c9f98969aead99
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-03-04 14:54:42 +0000

    Changed the test functions.

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -25,12 +25,12 @@ class TestBlogModel:
         primary_key = blog._meta.get_field("id").primary_key
         assert primary_key == True
 
-    def test_blog_max_length(self, setup_test_data):
+    def test_slug_max_length(self, setup_test_data):
         blog = setup_test_data
         max_length = blog._meta.get_field("slug").max_length
         assert max_length == 50
 
-    def test_blog_default_is_blank(self, setup_test_data):
+    def test_slug_default_is_blank(self, setup_test_data):
         blog = setup_test_data
         slug_default = blog._meta.get_field("slug").default
         assert slug_default == ""
