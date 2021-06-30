186458af3753717c57a834b56fe674e10ab6f093
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-06-30 13:46:16 +0100

    Revert back to unittest from pytest.

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,41 +1,35 @@
-import pytest
+from django.test import TestCase
 
 from blog.models import Blog
 
 
-@pytest.fixture
-def setup_test_data():
-    ident = 998
+class TestBlogModel(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Blog.blogs.create(
+            id=1,
+            slug="testing-blog-1",
+            title="Testing blog 1.",
+            date="2021-01-01",
+            content="Testing blog 1.",
+        )
 
-    Blog.blogs.create(
-        id=ident,
-        slug="testing-blog-1",
-        title="Testing blog 1.",
-        date="2021-01-01",
-        content="Testing blog 1.",
-    )
-
-    return Blog.blogs.get(id=ident)
-
-
-@pytest.mark.django_db
-class TestBlogModel:
-    def test_id_primary_key_is_true(self, setup_test_data):
-        blog = setup_test_data
+    def test_id_primary_key_is_true(self):
+        blog = Blog.blogs.get(id=1)
         primary_key = blog._meta.get_field("id").primary_key
-        assert primary_key == True
+        self.assertTrue(primary_key)
 
-    def test_slug_max_length(self, setup_test_data):
-        blog = setup_test_data
+    def test_slug_max_length(self):
+        blog = Blog.blogs.get(id=1)
         max_length = blog._meta.get_field("slug").max_length
-        assert max_length == 100
+        self.assertEquals(max_length, 100)
 
-    def test_slug_default_is_blank(self, setup_test_data):
-        blog = setup_test_data
+    def test_slug_default_is_blank(self):
+        blog = Blog.blogs.get(id=1)
         slug_default = blog._meta.get_field("slug").default
-        assert slug_default == ""
+        self.assertEquals(slug_default, "")
 
-    def test_title_max_length(self, setup_test_data):
-        blog = setup_test_data
+    def test_title_max_length(self):
+        blog = Blog.blogs.get(id=1)
         max_length = blog._meta.get_field("title").max_length
-        assert max_length == 100
+        self.assertEquals(max_length, 100)
--- blog/tests/test_views.py
+++ blog/tests/test_views.py
@@ -1,41 +1,28 @@
-import pytest
-
-from django.test import RequestFactory
-
-from blog.views import index, post_detail
+from django.test import TestCase
 
 from blog.models import Blog
 
 
-@pytest.fixture
-def setup_test_data():
-    ident = 998
-
-    for blog_number in range(3):
+class TestBlogView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
         Blog.blogs.create(
-            id=ident + blog_number,
-            slug=f"testing-blog-{blog_number}",
-            title=f"Testing blog {blog_number}.",
+            id=1,
+            slug="testing-blog-1",
+            title="Testing blog 1.",
             date="2021-01-01",
-            content=f"Testing blog {blog_number}.",
+            content="Testing blog 1.",
         )
 
+    def test_index_status_code_succeeded(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(
+            response,
+            "I'm Andy, a software developer with 15 plus years of real-life experience.",
+        )
 
-@pytest.mark.django_db
-class TestBlogView:
-    def setup_test_variables(self):
-        self.factory = RequestFactory()
-
-    def test_index_status_code_succeeded(self, setup_test_data):
-        self.setup_test_variables()
-        request = self.factory.get("/")
-
-        response = index(request)
-        assert response.status_code == 200
-
-    def test_post_detail_status_code_succeeded(self, setup_test_data):
-        self.setup_test_variables()
-        request = self.factory.get("/post/testing-2")
-
-        response = post_detail(request, "testing-blog-2")
-        assert response.status_code == 200
+    def test_post_detail_status_code_succeeded(self):
+        response = self.client.get("/post/testing-blog-1/")
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Testing blog 1")
